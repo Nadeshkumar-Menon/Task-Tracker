52497a5380077da338a6b161518f174881e27ffb
Nadeshkumar-Menon <menon.nadesh4@gmail.com>
2022-08-08 22:11:31 +0530

    Added Widgets
    Entries for username and password. Added styling to the window.

--- GUI.py
+++ GUI.py
@@ -8,8 +8,9 @@ Created on Sun Aug  7 20:11:39 2022
 #Creating an Image editor
 
 import tkinter as tk
+from tkinter import ttk
 
-window_dimension = '800x400'
+window_dimension = '500x400'
 
 
 class MainFrame:
@@ -18,14 +19,32 @@ class MainFrame:
     '''
     def __init__(self,master):
         self.master = master
-        self.frame = tk.LabelFrame(self.master,text = 'Login',bg = '#66ccff')
+        self.frame = tk.LabelFrame(self.master,text = 'Login',bg = '#66ccff',fg='white',font=('Helvetica Bold',14))
         self.frame.pack(fill = 'both',expand = True)
         
-        #Label to enter filename
+        #initialize label style
+        self.style = ttk.Style(self.master)
+        self.style.configure('TLabel',font=('Helvetica',14),foreground='white',background='#66ccff')
+        # self.style.configure('Tentry')
+        #Label to enter Username
+        self.uname_lbl = ttk.Label(self.frame,text='Username -')
+        self.uname_lbl.place(x=50,y=100)
+        #Label to enter Passsword
+        self.password_lbl = ttk.Label(self.frame,text='Password -')
+        self.password_lbl.place(x=50,y=150)
         
-        #Add a button for Action
+        #Entry for uname
+        self.uname_temp = ''
+        self.uname_entry = ttk.Entry(self.uname_temp,width=40)
+        self.uname_entry.place(x=220,y=128)
+        #Entry for uname
+        self.pass_temp = ''
+        self.pass_entry = ttk.Entry(self.pass_temp,width=40)
+        self.pass_entry.place(x=220,y=178)
         
-        #Add a close button
+        #Add a button for Login
+        
+        #add an action funtion
 
 
 
--- filterLogic.py
+++ filterLogic.py
@@ -5,11 +5,5 @@ Created on Sun Aug  7 20:17:11 2022
 @author: Nadeshkumar
 """
 
-from PIL import Image,ImageFilter
 
-path = r'image.jpg'
-
-with Image.open(path) as img :
-    # img.filter(ImageFilter.SHARPEN).show()
-    img.filter(ImageFilter.BLUR).show()
 
